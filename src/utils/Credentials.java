93ecdbdf340b44797600b1beb0d366231f1ab108
JuanManuelDS <juanmadesenzi@gmail.com>
2022-10-28 16:10:53 +0200

    veremos si funciona

---
+++ src/utils/Credentials.java
@@ -0,0 +1,21 @@
+package utils;
+
+public class Credentials {
+
+	private String usuario;
+	private String password;
+	
+	public Credentials() {
+		usuario = "remote";
+		password = "549Dijpa$!";
+	}
+	
+	public String getUsuario() {
+		return usuario;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+}
